1d08075df1328aa5722b5c06a6a7d0e8e34ab972
martazaryn <martazaryn@gmail.com>
2017-06-10 12:04:44 +0200

    Added show Slide function, speeding up other ones

--- gallery.js
+++ gallery.js
@@ -2,67 +2,58 @@ let currentNumber = 1;
 
 // Slide 1
 function showSlide1() {
-   document.querySelector('.show').classList.remove('show');
-   document.querySelector( '#slide1' ).classList.add('show');
+   showSlide(1);
 }
 
 document.querySelector('#pin1').addEventListener('click', showSlide1);
 
 // Slide 2
 function showSlide2() {
-   document.querySelector('.show').classList.remove('show');
-   document.querySelector( '#slide2' ).classList.add('show');
+   showSlide(2);
 }
 
 document.querySelector('#pin2').addEventListener('click', showSlide2);
 
 // Slide 3
 function showSlide3() {
-   document.querySelector('.show').classList.remove('show');
-   document.querySelector( '#slide3' ).classList.add('show');
+   showSlide(3);
 }
 
 document.querySelector('#pin3').addEventListener('click', showSlide3);
 
 // Slide 4
 function showSlide4() {
-   document.querySelector('.show').classList.remove('show');
-   document.querySelector( '#slide4' ).classList.add('show');
+   showSlide(4);
 }
 
 document.querySelector('#pin4').addEventListener('click', showSlide4);
 
 // Slide 5
 function showSlide5() {
-   document.querySelector('.show').classList.remove('show');
-   document.querySelector( '#slide5' ).classList.add('show');
+   showSlide(5);
 }
 
 document.querySelector('#pin5').addEventListener('click', showSlide5);
 
 function showNextSlide() {
-	document.querySelector('.show').classList.remove('show');
 	let newNumber = currentNumber + 1;
 	if (newNumber > 5)
 	{
 		newNumber = 1;
 	}
-	document.querySelector( '#slide' + newNumber ).classList.add('show');
-	currentNumber = newNumber;
+	showSlide(newNumber);
 
 }
 
 document.querySelector('#next').addEventListener('click', showNextSlide)
 
 function showPreviousSlide() {
-	document.querySelector('.show').classList.remove('show');
 	let newNumber = currentNumber - 1;
 	if (newNumber < 1)
 	{
 		newNumber = 5;
 	}
-	document.querySelector('#slide' + newNumber).classList.add('show');
-	currentNumber = newNumber;
+	showSlide(newNumber);
 }
 
 document.querySelector('#prev').addEventListener('click', showPreviousSlide)
@@ -72,5 +63,3 @@ function showSlide(newNumber) {
 	document.querySelector( '#slide' + newNumber ).classList.add('show');
 	currentNumber = newNumber;
 }
-
-//  zapisanie aktualnego numeru.
