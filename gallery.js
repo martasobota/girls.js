f178aa73e7fa4a3c1c0e15702eb8c53e8ee2a418
martazaryn <martazaryn@gmail.com>
2017-06-10 10:05:33 +0200

    Gallery

--- gallery.js
+++ gallery.js
@@ -2,6 +2,12 @@
  * Plik gallery.js
  * Tutaj będziemy dodawać nasz kod. 
  */
-document.querySelector( '#slide4' ).classList.add('show');
+// document.querySelector( '#slide4' ).classList.add('show');
 
-document.querySelector('#slide5').classList.remove('show');
+// document.querySelector('#slide5').classList.remove('show');
+
+
+function showSlide5() {
+   document.querySelector('.show').classList.remove('show');
+   document.querySelector( '#slide5' ).classList.add('show');
+}
