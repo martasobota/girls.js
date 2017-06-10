5bcb285e3de6e255f17a71fe1a6db98e2e937a88
martazaryn <martazaryn@gmail.com>
2017-06-10 10:00:58 +0200

    Gallery

---
+++ gallery.js
@@ -0,0 +1,7 @@
+/* girls.js - Interaktywna Galeria
+ * Plik gallery.js
+ * Tutaj będziemy dodawać nasz kod. 
+ */
+document.querySelector( '#slide4' ).classList.add('show');
+
+document.querySelector('#slide5').classList.remove('show');
