64e9dcb90ff3e60ceb4b567b42d70ff38b778aa0
martazaryn <martazaryn@gmail.com>
2017-06-10 12:28:19 +0200

    added for loop for changing pins slides

--- gallery.js
+++ gallery.js
@@ -1,39 +1,14 @@
 let currentNumber = 1;
+document.querySelector('#slide' + currentNumber).classList.add('show');
+document.querySelector( '#pin' + currentNumber ).classList.add('selected');
 
-// Slide 1
-function showSlide1() {
-   showSlide(1);
+for (let numerPinu = 1; numerPinu <= 5; numerPinu++) {
+	document
+		.querySelector('#pin' + numerPinu)
+		.addEventListener('click', function() {
+			showSlide(numerPinu);
+		});
 }
-
-document.querySelector('#pin1').addEventListener('click', showSlide1);
-
-// Slide 2
-function showSlide2() {
-   showSlide(2);
-}
-
-document.querySelector('#pin2').addEventListener('click', showSlide2);
-
-// Slide 3
-function showSlide3() {
-   showSlide(3);
-}
-
-document.querySelector('#pin3').addEventListener('click', showSlide3);
-
-// Slide 4
-function showSlide4() {
-   showSlide(4);
-}
-
-document.querySelector('#pin4').addEventListener('click', showSlide4);
-
-// Slide 5
-function showSlide5() {
-   showSlide(5);
-}
-
-document.querySelector('#pin5').addEventListener('click', showSlide5);
 
 function showNextSlide() {
 	let newNumber = currentNumber + 1;
@@ -62,4 +37,10 @@ function showSlide(newNumber) {
 	document.querySelector('.show').classList.remove('show');
 	document.querySelector( '#slide' + newNumber ).classList.add('show');
 	currentNumber = newNumber;
+	selectPin(newNumber);
+}
+
+function selectPin(newNumber) {
+	document.querySelector('.selected').classList.remove('selected');
+	document.querySelector('#pin' + newNumber).classList.add('selected');
 }
